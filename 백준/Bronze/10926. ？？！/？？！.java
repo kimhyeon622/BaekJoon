6af065732c002feba7ab5199ed5b3ecacd1c236b
kimhyeon622 <121088829+kimhyeon622@users.noreply.github.com>
2023-03-30 23:01:41 +0900

    [Bronze V] Title: ??!, Time: 204 ms, Memory: 17704 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		String A;
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("");
+		A = sc.next();
+		
+		System.out.print(A+"??!");
+		
+
+	}
+
+}
