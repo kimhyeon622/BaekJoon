ef300017a0c0b04d346fbe0bcb53ddf83b01cb01
kimhyeon622 <121088829+kimhyeon622@users.noreply.github.com>
2023-03-30 23:15:47 +0900

    [Bronze V] Title: A+B - 2, Time: 208 ms, Memory: 17704 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args){
+		// 입출력에 사용할 객체
+        Scanner sc = new Scanner(System.in);
+        
+        // 정수 A와B 입력 받기
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        
+        // A와B를 더한 값 출력
+        System.out.println(A+B);
+	}
+}
