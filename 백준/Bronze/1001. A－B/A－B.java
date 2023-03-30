fac9ad9b4ead54712016d1668f62ccb14da1a16e
kimhyeon622 <121088829+kimhyeon622@users.noreply.github.com>
2023-03-30 21:38:16 +0900

    [Bronze V] Title: A-B, Time: 204 ms, Memory: 17692 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int A;
+		int B;
+		int C;
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("");
+		A = sc.nextInt();
+		System.out.print("");
+		B = sc.nextInt();
+		
+		C = A - B;
+		System.out.print(C);
+		
+
+	}
+
+}
