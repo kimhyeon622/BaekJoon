cf954d3e889dc2f39a6801ecd2f53b5538476d92
kimhyeon622 <121088829+kimhyeon622@users.noreply.github.com>
2023-03-30 23:01:07 +0900

    [Bronze V] Title: 사칙연산, Time: 208 ms, Memory: 17788 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int A;
+		int B;
+		int C;
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("");
+		A = sc.nextInt();
+		System.out.print("");
+		B = sc.nextInt();
+		
+		C = A + B;
+		System.out.println(C);
+		
+		C = A - B;
+		System.out.println(C);
+		
+		C = A * B;
+		System.out.println(C);
+		
+		C = A / B;
+		System.out.println(C);
+		
+		C = A % B;
+		System.out.println(C);
+		
+
+	}
+
+}
