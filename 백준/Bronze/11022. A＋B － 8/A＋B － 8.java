0acd9f3806b06857621ef1a46bad42bb4f728104
kimhyeon622 <121088829+kimhyeon622@users.noreply.github.com>
2023-03-30 23:02:59 +0900

    [Bronze V] Title: A+B - 8, Time: 288 ms, Memory: 18960 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("");
+		int T = sc.nextInt();
+		
+		for (int i = 1; i <= T; i++) {
+			
+			System.out.print("");
+			int A = sc.nextInt();
+			System.out.print("");
+			int B = sc.nextInt();
+			
+			System.out.println("Case #"+i+": "+A+" + "+B+" = "+(A+B));
+		}
+		
+					
+	
+}}
