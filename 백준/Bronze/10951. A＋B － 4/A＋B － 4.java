16a71b52ccd5ee60761cbf618373507a59aed662
kimhyeon622 <121088829+kimhyeon622@users.noreply.github.com>
2023-03-30 23:02:05 +0900

    [Bronze V] Title: A+B - 4, Time: 240 ms, Memory: 17916 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		while (sc.hasNextInt()) {
+			System.out.print("");
+			int A = sc.nextInt();
+			
+			System.out.print("");
+			int B = sc.nextInt();
+			
+			System.out.println(A+B);
+			
+		}
+		
+		
+		
+		
+		
+					
+	
+}}
