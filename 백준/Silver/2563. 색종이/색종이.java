b844b7437156d5afb1f415287c4b352ac08b0472
kimhyeon622 <121088829+kimhyeon622@users.noreply.github.com>
2023-04-10 20:32:35 +0900

    [Silver V] Title: 색종이, Time: 220 ms, Memory: 17764 KB -BaekjoonHub

---
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args){
+		// 입출력에 사용할 객체
+        Scanner sc = new Scanner(System.in);
+        
+        // 도화지의 크기 100 * 100
+        int[][] arr = new int[100][100];
+        
+        int N = sc.nextInt();
+        
+        for(int i = 0; i < N; i++) {
+        	int Y = sc.nextInt();
+        	int X = sc.nextInt();
+        	// 각각 x,y부터 10*10의 크기로 색종이를 붙인다
+        	for(int y = Y; y < Y+10; y++) {
+        		for(int x = X; x < X+10; x++) {
+        			arr[y][x] = 1;
+        		}
+        	}
+        }
+        
+        // 색종이가 붙은 영역을 알기 위한 변수
+        int count = 0;
+        // 색종이가 붙은 검은 영역의 넓이 출력
+        for(int i = 0; i < 100; i++) {
+        	for(int j = 0; j < 100; j++) {
+        		// 색종이가 붙은 영역이라면 count를 증가시킨다
+            	if(arr[i][j] == 1) {
+            		count++;
+            	}
+        	}
+        }
+        
+        // 색종이가 붙은 검은 영역의 넓이
+        System.out.println(count);
+        
+	}
+}
