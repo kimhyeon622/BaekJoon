df2117cabe8bbed981d4b472c7d09ee74cb6a4e4
kimhyeon622 <121088829+kimhyeon622@users.noreply.github.com>
2023-04-15 22:21:35 +0900

    [Bronze V] Title: 콜센터, Time: 208 ms, Memory: 17096 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// 입출력에 사용할 객체
+		Scanner sc = new Scanner(System.in);
+		
+		// 콜센터에 앉아있는 C3PO 출력
+		System.out.println("     /~\\");
+		System.out.println("    ( oo|");
+		System.out.println("    _\\=/_");
+		System.out.println("   /  _  \\");
+		System.out.println("  //|/.\\|\\\\");
+		System.out.println(" ||  \\ /  ||");
+		System.out.println("============");
+		System.out.println("|          |");
+		System.out.println("|          |");
+		System.out.println("|          |");
+
+	}
+}
