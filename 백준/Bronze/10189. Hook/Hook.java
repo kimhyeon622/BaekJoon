08ac071f8a98c9f92bfb295f121e378e038d1662
kimhyeon622 <121088829+kimhyeon622@users.noreply.github.com>
2023-04-23 00:01:11 +0900

    [Bronze V] Title: Hook, Time: 120 ms, Memory: 13888 KB -BaekjoonHub

---
+++ 백준/Bronze/10189. Hook/Hook.java
@@ -0,0 +1,12 @@
+public class Main {
+
+	public static void main(String[] args) {
+
+		// HOOK 출력
+		System.out.println("#  # #### #### #  #");
+		System.out.println("#### #  # #  # # #");
+		System.out.println("#### #  # #  # # #");
+		System.out.println("#  # #### #### #  #");
+		
+	}
+}
