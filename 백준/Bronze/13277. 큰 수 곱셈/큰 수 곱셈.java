7b0fcaa17040ac42dcae524040b256f3e8f0d5cd
kimhyeon622 <121088829+kimhyeon622@users.noreply.github.com>
2023-04-23 00:08:38 +0900

    [Bronze V] Title: 큰 수 곱셈, Time: 6128 ms, Memory: 408220 KB -BaekjoonHub

---
+++ 백준/Bronze/13277. 큰 수 곱셈/큰 수 곱셈.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		
+		// 입출력에 사용할 객체
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		// 입력받기
+		String[] arr = br.readLine().split(" ");
+		
+		// 정수 : A, B
+		BigInteger A = new BigInteger(arr[0]);
+		BigInteger B = new BigInteger(arr[1]);
+		
+		// A 곱하기 B 하여 출력
+		System.out.println(A.multiply(B));
+		
+	}
+}
