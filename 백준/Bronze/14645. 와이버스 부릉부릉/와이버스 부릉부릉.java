0da1f45ac7cad8230cbb2268ec3eaee6628b54bf
kimhyeon622 <121088829+kimhyeon622@users.noreply.github.com>
2023-04-23 00:18:07 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 116 ms, Memory: 13876 KB -BaekjoonHub

--- 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.java
+++ 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.java
@@ -2,7 +2,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		// 입력으로 무엇을 받든 "비와이"출력
+		// 입력을 받든 안받든 "비와이"를 출력하기 때문에
+        // "비와이"출력문만 있으면 된다
 		System.out.println("비와이");
 
 		
