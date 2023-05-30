8ce1a7f035dfedf774ae616881036afd9a209e9c
kimhyeon622 <121088829+kimhyeon622@users.noreply.github.com>
2023-05-30 19:40:42 +0900

    [Bronze V] Title: Арифметическая магия, Time: 124 ms, Memory: 14080 KB -BaekjoonHub

---
+++ 백준/Bronze/18096. Арифметическая магия/Арифметическая магия.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		
+		// 입출력 객체
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		// N이 몇이 나오든 1이 된다
+		System.out.println(1);
+		
+	}	
+}
